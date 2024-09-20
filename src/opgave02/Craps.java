ed5be5c27134076420d16dd7884fdc191a1d4486
datso <datsomor321@gmail.com>
2024-09-20 11:26:03 +0200

    opgave 1 og noget af 3

--- src/opgave02/Craps.java
+++ src/opgave02/Craps.java
@@ -1,4 +1,111 @@
 package opgave02;
 
-public class Craps {
+import java.util.Scanner;
+
+public class Craps
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Craps");
+        printRules();
+        System.out.println();
+        playCraps();
+        System.out.println();
+    }
+
+    public static void printRules()
+    {
+        System.out.println("========================================");
+        System.out.println("The player rolls two six-sided die");
+        System.out.println("The sum of the two die are calculated");
+        System.out.println("If the sum is 7 or 11 on the first throw, the player wins");
+        System.out.println("If the sum is 2,3, or 12 on the first throw, this is called Craps, and the player loses");
+        System.out.println("If the sum is 4,5,6,8,9 or 10 on the first throw, that becomes the players point");
+        System.out.println("The player must keep rolling until they make their point");
+        System.out.println("The player loses if they roll a 7 before making their point");
+        System.out.println("========================================");
+    }
+
+    public static void playCraps()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Play Craps?? ('Yes/No') ");
+        String answer = scanner.nextLine();
+
+        while (!answer.equals("No"))
+        {
+            int[] rolledDice = rollTwoDice();
+            int sumOfDie = rolledDice[0] + rolledDice[1];
+            System.out.println("You rolled = " + sumOfDie);
+
+            if (sumOfDie == 7 || sumOfDie == 11)
+            {
+                System.out.println("You rolled " + sumOfDie + ", You are a winner!");
+            }
+            else if (sumOfDie == 2 || sumOfDie == 3 || sumOfDie == 12)
+            {
+                System.out.println("Aw shucks! You rolled " + sumOfDie + " You lose!");
+            }
+            else{
+                System.out.println("Your point is " + sumOfDie + ". You need to again to hit your point");
+                rollForPoint(sumOfDie);
+            }
+            playAgainAsk();
+        }
+    }
+
+    public static void rollForPoint(int currentPoint)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.print("Roll again? ('Yes/No') ");
+            String answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("No"))
+            {
+                System.out.println("Thanks for playing Craps!");
+                break;
+            }
+
+            int[] rolledDice = rollTwoDice();
+            int sumOfNewDice = rolledDice[0] + rolledDice[1];
+            System.out.println("You rolled a " + sumOfNewDice);
+
+            if (sumOfNewDice == currentPoint)
+            {
+                System.out.println("You hit your point! You win!");
+                break;
+            } else if (sumOfNewDice == 7)
+            {
+                System.out.println("You rolled a 7. You lose!");
+                break;
+            }
+        }
+    }
+
+    public static int[] rollTwoDice()
+    {
+        int rollDieOne = (int) (Math.random() * 6 + 1);
+        int rollDieTwo = (int) (Math.random() * 6 + 1);
+        int[] diceArray = {rollDieOne, rollDieTwo};
+        return diceArray;
+    }
+
+    public static void playAgainAsk()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to play again? ('Yes/No') ");
+        String answer = scanner.nextLine();
+        if (answer.equalsIgnoreCase("Yes"))
+        {
+            playCraps();
+        }
+        else
+        {
+            System.out.println("Thanks for playing Craps");
+        }
+    }
 }
--- src/opgave03/Pigs.java
+++ src/opgave03/Pigs.java
@@ -5,42 +5,141 @@ import java.util.Scanner;
 public class Pigs {
     private static int playerOneSum = 0;
     private static int playerTwoSum = 0;
+    private static double playerOneThrows = 0;
+    private static double playerTwoThrows = 0;
+    private static double playerOneTurns = 0;
+    private static double playerTwoTurns = 0;
+    private static double playerOneAverage = 0;
+    private static double playerTwoAverage = 0;
+
 
     public static void main(String[] args) {
-
+        playPig();
     }
 
     private static void playPig() {
+        System.out.println("Type winner value");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Rul en terning? ('ja/nej') ");
-        String answer = scanner.nextLine();
-        while (!answer.equals("nej")) {
-            int face = rollDie();
-            System.out.println("Du rullede: " + face);
-            System.out.println();
+        int winnerValue = scanner.nextInt();
 
-            System.out.print("Rul en terning? ('ja/nej') ");
+        while (playerOneSum < winnerValue || playerTwoSum < winnerValue){
+           playerOne(winnerValue);
+           playerOneTurns++;
+
+           if(playerOneSum >= winnerValue){
+               break;
+           }
+           playerTwo(winnerValue);
+           playerTwoTurns++;
+           if(playerTwoSum >= winnerValue){
+               break;
+           }
+        }
+
+        if(playerOneSum > playerTwoSum){
+            System.out.println("Player 1 wins!!!");
+        }
+        else {
+            System.out.println("Player 2 wins!!!");
+        }
+        updateStatistics();
+        System.out.println("========================================");
+        System.out.println("Statistics");
+        System.out.println("Player one throws pr. turn: " + playerOneAverage);
+        System.out.println("Player Two trows pr. turn: " + playerTwoAverage);
+        System.out.println("========================================");
+        System.out.println("Thank you for playing :)");
+    }
+
+    private static int[] rollDice() {
+        int die1 = (int) (Math.random() * 6 + 1);
+        int die2 = (int) (Math.random() * 6 + 1);
+        int[] diceArray = {die1,die2};
+        return diceArray;
+    }
+
+    private static void playerOne(int winnerValue){
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        int currentSum = 0;
+        System.out.println("Player 1.: your total is " + playerOneSum);
+        System.out.println("Roll dice? (yes/no)");
+        answer = scanner.nextLine();
+        while (!answer.equals("no")) {
+            currentSum = 0;
+            int[] faces = rollDice();
+            System.out.print("You rolled: ");
+            for (int i = 0; i < faces.length; i++){
+                System.out.print(faces[i]+ " ");
+                if(i<1){
+                    System.out.print("and ");
+                }
+                currentSum+=faces[i];
+            }
+            playerOneThrows++;
+
+            if(faces[0] == 1 || faces[1] == 1){
+                currentSum = 0;
+                break;
+            }
+            else if(faces[0] == 1 && faces[1] == 1){
+                currentSum = 0;
+                playerOneSum = 0;
+                break;
+            }
+            playerOneSum+=currentSum;
+            System.out.println("Total: " + playerOneSum);
+            if(playerOneSum >= winnerValue){
+                break;
+            }
+            System.out.println("Roll again? (yes/on)");
             answer = scanner.nextLine();
         }
-        scanner.close();
+        System.out.println("Your turn is over... your total is " + playerOneSum);
     }
 
-    private static int rollDie() {
-        return (int) (Math.random() * 6 + 1);
+    private static void playerTwo(int winnerValue){
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        int currentSum = 0;
+        System.out.println("Player 2.: your total is " + playerTwoSum);
+        System.out.println("Roll dice? (yes/no)");
+        answer = scanner.nextLine();
+        while (!answer.equals("no")) {
+            currentSum = 0;
+            int[] faces = rollDice();
+            System.out.print("You rolled: ");
+            for (int i = 0; i < faces.length; i++){
+                System.out.print(faces[i]+ " ");
+                if(i<1){
+                    System.out.print("and ");
+                }
+                currentSum+=faces[i];
+            }
+            playerTwoThrows++;
+
+            if(faces[0] == 1 || faces[1] == 1){
+                currentSum = 0;
+                break;
+            }
+            else if(faces[0] == 1 && faces[1] == 1){
+                currentSum = 0;
+                playerTwoSum = 0;
+                break;
+            }
+            playerTwoSum+=currentSum;
+            System.out.println("Total: " + playerTwoSum);
+            if(playerTwoSum >= winnerValue){
+                break;
+            }
+            System.out.println("Roll again? (yes/on)");
+            answer = scanner.nextLine();
+        }
+        System.out.println("Your turn is over... your total is " + playerTwoSum);
     }
 
-    private static void playerOne(){
-        System.out.println("Player 1. \n your total is " + playerOneSum);
-    }
-
-    private static void playerTwo(){
-        System.out.println("Player 2. \n your total is " + playerTwoSum);
-    }
-
-    private static void printRules() {
-        System.out.println("=====================================================");
-        System.out.println("Regler for rul en terning");
-        System.out.println("Spilleren ruller en terning, så længde man lyster.");
-        System.out.println("=====================================================");
+    private static void updateStatistics(){
+        playerOneAverage = playerOneThrows/playerOneTurns;
+        playerTwoAverage = playerTwoThrows/playerTwoTurns;
     }
 }
